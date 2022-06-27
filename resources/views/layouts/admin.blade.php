acdc263578c2cb39c0d612c17a65880cef5c742b
xeryz05 <xierradepan01@gmail.com>
2022-06-28 06:18:42 +0700

    menambah styling dan script datatables

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,7 @@
     @stack('prepend-style')
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <link href="style/main.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.12.1/datatables.min.css"/>
     @stack('addon-style')
   </head>
 
@@ -28,7 +29,7 @@
           </div>
           <div class="list-group list-group-flush">
             <a
-              href="{{ route('dashboard') }}"
+              href="{{ route('admin-dashboard') }}"
               class="list-group-item list-group-item-action"
               >Dashboard</a
             >
@@ -144,8 +145,9 @@
 
     <!-- Bootstrap core JavaScript -->
     @stack('prepend-script')
-    <script src="/vendor/jquery/jquery.slim.min.js"></script>
+    <script src="/vendor/jquery/jquery.min.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.12.1/datatables.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
       AOS.init();
