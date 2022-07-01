652da3de9ac266bc5c7dccdc9a89fea0ed1b42e8
xeryz05 <xierradepan01@gmail.com>
2022-07-01 08:09:10 +0700

    detail book(done) progres selanjutnya pdf(0%)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,19 +17,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/categories', 'CategoryController@index')->name('categories');
+Route::get('/categories/{id}', 'CategoryController@detail')->name('categories-detail');
 Route::get('/detail/{id}', 'DetailController@index')->name('detail');
 Route::get('/read/{id}', 'ReadController@index')->name('read');
 
 
 Route::get('/register/success', 'Auth\RegisterController@success')->name('register-success');
 
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
-Route::get('/dashboard-read', 'DashboardReadController@index')->name('dashboard-read');
-Route::get('/dashboard-read-details', 'DashboardReadDetailsController@index')->name('dashboard-read-details');
-Route::get('/dashboard-account', 'DashboardAccountController@index')->name('dashboard-account');
+
+Route::group(['Middleware'=>['auth']], function(){
+    Route::get('/dashboard', 'DashboardController@index')
+        ->name('dashboard');
+    Route::get('/dashboard-read', 'DashboardReadController@index')
+        ->name('dashboard-read');
+    Route::get('/dashboard-read-details', 'DashboardReadDetailsController@index')->name('dashboard-read-details');
+
+    Route::get('/dashboard-account', 'DashboardAccountController@index')
+        ->name('dashboard-account');
+
+});
 
 Route::prefix('admin')
     ->namespace('Admin')
+    ->middleware(['auth','admin'])
     ->group(function (){
         Route::get('/', 'DashboardController@index')->name('admin-dashboard');
         Route::resource('category', 'CategoryController');
